6047a302059ed9b94d333aaff9a9e7dc6660e8d5
Lija Majevska <lija.majevska@ventspils.edu.lv>
2025-01-16 08:27:38 +0200

    Veiktas izmainas1

--- Darbs.py
+++ Darbs.py
@@ -1,1 +1,9 @@
+class Dzivnieks:
+    def __init__(self, vards, kajas):
+        self.vards = vards
+        self.kajas = kajas
+    def skanja(self):
+        print("random animal noise")
 
+d1 = Dzivnieks("Gauja", 4)
+d1.skanja()
