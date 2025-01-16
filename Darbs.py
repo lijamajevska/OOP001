0c3e48baa65c28ed3cc4ed7b4cf411b263a9e8bc
Lija Majevska <lija.majevska@ventspils.edu.lv>
2025-01-16 08:54:12 +0200

    Suns

--- Darbs.py
+++ Darbs.py
@@ -4,6 +4,23 @@ class Dzivnieks:
         self.kajas = kajas
     def skanja(self):
         print("random animal noise")
+    def __str__(self):
+        return f"{self.vards} un {self.kajas} kajas"
+        
+
+class Suns(Dzivnieks):
+    def __init__(self, vards, kajas):
+        super().__init__(vards, kajas)
+        self.vards = "Komisars" + self.vards
+    def skanja(self):
+        print("Vau! Vau! Gaf! Gaf!")
+
 
 d1 = Dzivnieks("Gauja", 4)
 d1.skanja()
+print(d1)
+d1.skanja()
+
+s1 = Suns("Reksis", 4)
+print(s1)
+s1.skanja()
