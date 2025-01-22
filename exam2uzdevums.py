f279a8455c04336e3f095c5f2defbba37473c27f
Lija Majevska <lija.majevska@ventspils.edu.lv>
2025-01-22 16:03:26 +0200

    veiktas izmainas

--- exam2uzdevums.py
+++ exam2uzdevums.py
@@ -5,13 +5,18 @@ class Skolotajs:
     klase = "x.i"
 
 class SakumskolasSkolotajs(Skolotajs):
+    klase = "x.i"
     def __init__(self):
         self.skolotajaTips = 1
     def ievade(self):
-        self.uzvards = input("Ievadiet uzvārdu --->")
+        self.uzvards = input("Ievadiet sākumskolas skolotāja uzvārdu ---> ")
+        self.klase =input("Ievadiet klasi ---> ")
+        self.stunduSkaits = input("Ievadiet stundu skaitu ---> ")
     def izvade(self):
         print(f"Sākumskolas (tips -{self.skolotajaTips}")
 class VidusskolasSkolotajs(Skolotajs):
+    def __init__(self): 
+        self.skolotajaTips = 3
     pirmaisPrieksmets = "x prieksmets"
     otraisPrieksmets = "y prieksmets"
     pirmaisPrieksmetsStundas = 0
@@ -19,9 +24,18 @@ class VidusskolasSkolotajs(Skolotajs):
     def cikStundasKopa(self):
         self.StunduSkaitsNedela = self.pirmaisPrieksmetsStundas + self.otraisPrieksmetsStundas
         return self.stunduSkaitsNedela
+    def ievade(self):
+        self.uzvards = input("Ievadiet vidusskolas skolotāja uzvārdu ---> ")
+        self.pirmaisPrieksmets = input("Ievadiet pirmo priekšmetu ---> ")
+        self.otraisPrieksmets = input("Ievadiet otro priekšmetu ---> ")
+        self.pirmaisPrieksmetsStundas = int(input("Ievadiet pirmā priekšmeta kopstundu skaitu ---> "))
+        self.otraisPrieksmetsStundas = int(input("Ievadiet otrā priekšmeta kopstundu skaitu ---> "))
     def izvade(self):
-        print(f"Vidusskolas (tips -{self.skolotajaTips}")
+        print(f"Vidusskolas (tips -{self.skolotajaTips} skolotājs {self.uzvards} māca šādus priekšmetus ---> ")
+        print(f"{self.pirmaisPrieksmets} un {self.otraisPrieksmets}, kopā {self.cikStundasKopa()} stundas.")
 ss1 = SakumskolasSkolotajs()
+ss1.ievade()
 ss1.izvade()
 vs1 = VidusskolasSkolotajs()
+vs1.ievade()
 vs1.izvade()
